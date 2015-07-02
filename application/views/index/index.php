f7b67cd7d919a67d6f5bcd816b3d43ab3bb84965
Byron Peña <byronpenna@gmail.com>
2015-07-02 08:02:16 -0600

    Diseño de login

---
+++ application/views/index/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<?php $this->load->view("parts/universalInclude.php") ?>
+	<!-- estilos -->
+		<link rel="stylesheet" type="text/css" href=<?php echo base_url("resources/css/index/style.css") ?> >
+		<link rel="stylesheet" type="text/css" href=<?php echo base_url("resources/css/index/media.css") ?> >
+	<!-- scripts -->
+		<script type="text/javascript" src=<?php echo base_url("resources/js/index/script.js") ?> ></script>
+		<script type="text/javascript" src=<?php echo base_url("resources/js/index/functions.js") ?> ></script>
+</head>
+<body>
+	<div class = "container">
+		<div class="wrapper">
+			<form action="" method="post" name="Login_Form" class="form-signin">       
+			    <h3 class="form-signin-heading">Welcome Back! Please Sign In</h3>
+				  <hr class="colorgraph"><br>
+				  
+				  <input type="text" class="form-control" name="Username" placeholder="Username" required="" autofocus="" />
+				  <input type="password" class="form-control" name="Password" placeholder="Password" required=""/>     		  
+				 
+				  <button class="btn btn-lg btn-primary btn-block"  name="Submit" value="Login" type="Submit">Login</button>  			
+			</form>			
+		</div>
+	</div>
+</body>
+</html>
---
+++ application/views/parts/universalInclude.php
@@ -0,0 +1,14 @@
+<!-- SCRIPTS -->
+	<!-- libs -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+	<!-- propios -->
+		<link rel="stylesheet" type="text/css" href=<?php echo base_url("resources/css/global/style.css") ?>>
+		<link rel="stylesheet" type="text/css" href=<?php echo base_url("resources/css/global/media.css") ?>>
+<!-- SCRIPTS -->
+	<!-- libs -->
+		<script type="text/javascript" src=<?php echo base_url("resources/third-party/jquery/jquery-1.11.3.min.js") ?> ></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<!-- propios -->
+		<script type="text/javascript" src=<?php echo base_url("resources/js/global/script.js") ?> ></script>
+		<script type="text/javascript" src=<?php echo base_url("resources/js/global/functions.js") ?> ></script>
